b280daa1dbf19f218129517614af1b10a9b463cb
elivelton <elliveltonboxfilewf6@hotmail.com>
2021-05-20 10:55:59 -0300

    Enviar dados relativos ao pagamento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,5 +2,8 @@ module.exports = app => {
 
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimento'))
 
-    app.post('/atendimentos', (req, res) => res.send('Você está na rota de atendimento com um POST'))
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Você está na rota de atendimento com um POST')
+    })
 }
